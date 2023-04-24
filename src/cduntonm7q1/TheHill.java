06f04311ac2f4e1fbcec50c07458c7dfbeb30104
Cory <ccd2138@email.vccs.edu>
2023-04-24 17:24:31 -0400

    created TheHill

---
+++ src/cduntonm7q1/TheHill.java
@@ -0,0 +1,21 @@
+package cduntonm7q1;
+
+// This program reads in "The Hill We Climb" poem.
+
+// Program should create an ArrayList<String> that stores the poem line by line.
+// Have it print out the number of lines in the poem
+// Next ask the user how many of the lines they want to process
+// Then have it print out the first word of this many lines of the poem.
+// Make words lower-case
+
+public class TheHill {
+	
+	// Need FileDialog for saving txt file
+	//import java.awt.FileDialog;
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
