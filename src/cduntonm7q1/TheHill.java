7ede93add04982b4fc2307f0ddcd05ad18ef9976
Cory <116191245+corydunton@users.noreply.github.com>
2023-04-25 16:34:31 -0400

    started

--- src/cduntonm7q1/TheHill.java
+++ src/cduntonm7q1/TheHill.java
@@ -8,13 +8,51 @@ package cduntonm7q1;
 // Then have it print out the first word of this many lines of the poem.
 // Make words lower-case
 
+
+// Need FileDialog for saving txt file
+import java.awt.FileDialog;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import readWriteFiles.FileLoc;
+
 public class TheHill {
 	
-	// Need FileDialog for saving txt file
-	import java.awt.FileDialog;
+
 
 	public static void main(String[] args) {
-
+		
+		Scanner scan = new Scanner(System.in);
+		ArrayList<String> string = new ArrayList<String>();
+		// Get filename
+		String theHillWeClimb = FileLoc.pickLocRead();
+		// Open file
+		File file = new File(theHillWeClimb);
+		Scanner inputFile = null;
+		int counter = 0;
+		
+		try {
+			inputFile = new Scanner(file);
+			
+			// Read lines from file
+			while (inputFile.hasNext()) {
+				String line = inputFile.nextLine();
+				if (!line.isEmpty()) {
+					string.add(line);
+					System.out.println(line);
+					counter++;
+				}
+			}
+		} catch (IOException ioe) {
+			System.out.println("IO Exception. Need to exit");
+			System.out.println(ioe.getMessage());
+			System.exit(0);
+		} finally {
+			inputFile.close();
+		}
+		
+		System.exit(0);
 	}
-
 }
