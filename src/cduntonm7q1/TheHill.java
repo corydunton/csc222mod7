acfb7607868398b8e0546b22bbb9000f3f43e9d7
Cory <ccd2138@email.vccs.edu>
2023-04-25 20:33:53 -0400

    formatting - finished

--- src/cduntonm7q1/TheHill.java
+++ src/cduntonm7q1/TheHill.java
@@ -1,15 +1,14 @@
 package cduntonm7q1;
 
-// This program reads in "The Hill We Climb" poem.
+// This program reads in "The Hill We Climb" poem
 
-// Program should create an ArrayList<String> that stores the poem line by line.
+// Program should create an ArrayList<String> that stores the poem line by line
 // Have it print out the number of lines in the poem
 // Next ask the user how many of the lines they want to process
 // Then have it print out the first word of this many lines of the poem.
 // Make words lower-case
 
-
-// Need FileDialog for saving txt file
+// Need FileDialog for opening txt file
 import java.awt.FileDialog;
 import java.io.File;
 import java.io.IOException;
@@ -19,29 +18,27 @@ import java.util.Scanner;
 import readWriteFiles.FileLoc;
 
 public class TheHill {
-	
-
 
 	public static void main(String[] args) {
-		
 		Scanner scan = new Scanner(System.in);
-		ArrayList<String> string = new ArrayList<String>();
+		ArrayList<String> poem = new ArrayList<String>();
+
 		// Get filename
 		String theHillWeClimb = FileLoc.pickLocRead();
+
 		// Open file
 		File file = new File(theHillWeClimb);
 		Scanner inputFile = null;
 		int counter = 0;
-		
+
 		try {
 			inputFile = new Scanner(file);
-			
+
 			// Read lines from file
 			while (inputFile.hasNext()) {
 				String line = inputFile.nextLine();
 				if (!line.isEmpty()) {
-					string.add(line);
-					System.out.println(line);
+					poem.add(line);
 					counter++;
 				}
 			}
@@ -52,7 +49,21 @@ public class TheHill {
 		} finally {
 			inputFile.close();
 		}
-		
+
+		System.out.println("The number of non-blank lines in the poem: " + counter);
+		System.out.println("In how many lines do you want to see the first word?");
+		int numLines = scan.nextInt();
+		System.out.println("First words of the first " + numLines + " lines (lower-case):");
+		printLines(poem, numLines);
+
 		System.exit(0);
+
+	}
+
+	public static void printLines(ArrayList<String> poem, int numLines) {
+		for (int i = 0; i < numLines; i++) {
+			String[] words = poem.get(i).split(" ");
+			System.out.print(words[0].toLowerCase() + " ");
+		}
 	}
 }
